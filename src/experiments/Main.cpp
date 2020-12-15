11621f47c0d235eec69e4e195423a71edd9f324d
avail <avail@pomf.se>
2020-12-15 15:59:46 +0100

    credit where credit is due

--- src/experiments/Main.cpp
+++ src/experiments/Main.cpp
@@ -109,7 +109,7 @@ DWORD WINAPI OnAttach(LPVOID lpParameter)
 		injector::MakeNOP(locations.get(0).get<char>(16), 2); // dbg #2
 	}
 
-	// skip crc hash verification while loading archives
+	// skip crc hash verification while loading archives (found by Snazz#3248)
 	injector::WriteMemory<BYTE>(hook::get_pattern("8B F8 85 C0 74 36 48 8B CB", 4), 0xEB, true);
 	injector::WriteMemory<BYTE>(hook::get_pattern("48 3B C1 74 0A B8 03 00 00 00 E9 6C 03 00 00", 2), 0xC0, true);
 	injector::WriteMemory<BYTE>(hook::get_pattern("48 3B C1 74 0A B8 03 00 00 00 E9 A2 01 00 00", 2), 0xC0, true);
